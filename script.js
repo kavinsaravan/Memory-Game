f2f74055019bbcced9952a8e9858ec97b10284c7
kavins <>
2021-04-11 00:15:38 +0000

    t

--- script.js
+++ script.js
@@ -16,9 +16,11 @@ images[12] = "cheetah.jpg"
 images[13] = "elephant.jpg"
 images[14] = "monkey.jpeg"
 images[15] = "frog.jpg"
+
+shuffle(images)
+
 // 1. go through all the cells in the table
 var table = document.getElementById("gametable")
-
 
 // 2. for each cell, attach the onclick listener
 var x = 0
@@ -27,7 +29,7 @@ for (i = 0; i < table.rows.length; i++){
   for (j = 0; j < row.cells.length; j++){
     var cell = table.rows[i].cells[j]
     //cell.style.backgroundColor = "green"
-    cell.kavin = "row " + i + " / col " + j
+    cell.kavin = "row " + i + " / col " + j 
     //console.log("CELL: " + cell.kavin)
     cell.onclick = flipCell
 
@@ -35,34 +37,74 @@ for (i = 0; i < table.rows.length; i++){
     //console.log("IMAGE: " + cell.style.backgroundImage)
     cell.style.backgroundPosition = "center center"
     cell.style.backgroundSize = "cover"
-    cell.tag = x
+    cell.id = x
+    cell.setAttribute("image", images[x])
+    //cell.tag = x
     x++
   }
 }
 
 // global variables
 var count = 0
-var previous = null
+var previousTag = ""
 var score = 0
 
+function resetCells(prevTag, currTag) {
+    var prev = document.getElementById(prevTag)
+    var curr = document.getElementById(currTag)
+    var currImage = curr.getAttribute("image")
+    var prevImage = prev.getAttribute("image")
+    if (currImage != prevImage) {
+        curr.style.backgroundImage = null
+        prev.style.backgroundImage = null
+    }
+}
+var waiting = false
 // called on each cell click
 function flipCell() {
-    this.style.backgroundImage = "url('" + images[this.tag] + "')"
+    if (waiting == true) {
+        return 
+    }
+    var currentTag = this.id
+    //console.log("CT: " + currentTag)
+    var current = document.getElementById(currentTag)
+    var isDone = current.getAttribute("done")
+    if (isDone == "true") {
+      return
+    }
+    var tagIndex = parseInt(currentTag)
+    this.style.backgroundImage = "url('" + images[tagIndex] + "')"
     count++
     if ((count % 2) == 0) {
-        var current = this
-        if (current.style.backgroundImage ==     previous.style.backgroundImage) {
+        var currentImage = this.getAttribute("image")
+        var previous = document.getElementById(previousTag)
+        var previousImage = previous.getAttribute("image")
+        if (currentImage == previousImage) {
             score = score + 10
             document.getElementById("score").innerHTML = score
-            
+            current.setAttribute("done", "true")
+            previous.setAttribute("done", "true")
+        } else {
+          waiting = true
+          var delayedClear = setTimeout(function() {
+              resetCells(previousTag, currentTag)
+              waiting = false
+          }, 500);
         }
-        setTimeout(function() {
-            if (current.style.backgroundImage !=     previous.style.backgroundImage) {
-                current.style.backgroundImage = null
-                previous.style.backgroundImage = null
-            } 
-        }, 1000);
     } else {
-        previous = this
+        if (delayedClear != undefined) {
+           clearTimeout(delayedClear)
+           resetCells(previousTag, currentTag)
+        }
+        previousTag = currentTag
+        
     }
+}
+
+function shuffle(a) {//this is the function shuffle
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1)); 
+        [a[i], a[j]] = [a[j], a[i]]; 
+    }
+    return a; //return the product
 }
